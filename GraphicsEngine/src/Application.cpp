48cbcd0148eb4b06c29ecfb6ef97ec6b1a465b6a
YajatKaul <68262974+yajatkaul@users.noreply.github.com>
2024-07-10 18:24:18 +0530

    Shifted the color scheme to cpu side

--- GraphicsEngine/src/Application.cpp
+++ GraphicsEngine/src/Application.cpp
@@ -5,6 +5,23 @@
 #include <string>
 #include <sstream>
 
+#define ASSERT(x) if(!(x)) __debugbreak();
+#define GLCall(x) GLClearError();\
+    x;\
+    ASSERT(GLCheckError(#x, __FILE__, __LINE__)) //# makes it string, __Name__ calls that variable
+
+static void GLClearError() {
+    while (glGetError() != GL_NO_ERROR); //!glGetError
+}
+
+static bool GLCheckError(const char* function, const char* file, int line) {
+    while (GLenum error = glGetError()) {
+        std::cout << "[OpenGL Error] (" << error << ")" << function << " " << file << ":" << line << std::endl;
+        return false;
+    }
+    return true;
+}
+
 struct ShaderSources {
     std::string VertexSource;
     std::string FragmentSource;
@@ -52,7 +69,7 @@ static unsigned int ComplieShader(unsigned int type, const std::string& source) 
         glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
         char* message =  (char*)alloca(length * sizeof(char));
         glGetShaderInfoLog(id, length, &length, message);
-        std::cout << "Failed to complie shader" << (type == GL_VERTEX_SHADER  ? "vertix" : "fragment") << std::endl;
+        std::cout << "Failed to complie shader " << (type == GL_VERTEX_SHADER  ? "vertix" : "fragment") << std::endl;
         std::cout << message << std::endl;
         glDeleteShader(id);
         return 0;
@@ -97,6 +114,8 @@ int main(void)
     /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
+    glfwSwapInterval(1); //Vsync on
+
     /* Checking if glew is working fine IMPORTANT HAS TO BE ADDED AFTER CONTEXT*/
     if (glewInit() != GLEW_OK) {
         std::cout << "Error!" << std::endl;
@@ -119,18 +138,18 @@ int main(void)
 
 
     unsigned int buffer;
-    glGenBuffers(1, &buffer);
-    glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
+    GLCall(glGenBuffers(1, &buffer));
+    GLCall(glBindBuffer(GL_ARRAY_BUFFER, buffer));
+    GLCall(glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW));
 
-    glEnableVertexAttribArray(0);
+    GLCall(glEnableVertexAttribArray(0));
 
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
- 
+    GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
+
     unsigned int ibo;
-    glGenBuffers(1, &ibo);
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(float), indices, GL_STATIC_DRAW);
+    GLCall(glGenBuffers(1, &ibo));
+    GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+    GLCall(glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(float), indices, GL_STATIC_DRAW));
 
     ShaderSources source = ParseShader("res/shaders/BasicShader.shader");
     std::cout << "VERTEX" << std::endl;
@@ -139,8 +158,14 @@ int main(void)
     std::cout << source.FragmentSource << std::endl;
 
     unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
-    glUseProgram(shader);
+    GLCall(glUseProgram(shader));
 
+    GLCall(int location = glGetUniformLocation(shader, "u_Color"));
+    ASSERT(location != -1);
+    GLCall(glUniform4f(location, 0.0f, 0.8f, 1.0f, 1.0f));
+
+    float r = 0.0f;
+    float increment = 0.05f;
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
@@ -148,8 +173,18 @@ int main(void)
         glClear(GL_COLOR_BUFFER_BIT);
 
         //glDrawArrays(GL_TRIANGLES, 0, 6);
-
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+        
+        GLCall(glUniform4f(location, r, 0.8f, 1.0f, 1.0f));
+        GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+        
+        if (r > 1.0f) {
+            increment = -0.05f;
+        }
+        else if(r < 0.0f){
+            increment = 0.05f;
+        }
+        
+        r += increment;
 
         /* Legacy
         glBegin(GL_TRIANGLES);
