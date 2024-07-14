032b5e010533e3789e04480ad9d6a1793a3499f8
YajatKaul <68262974+yajatkaul@users.noreply.github.com>
2024-07-14 22:46:13 +0530

    modified:   GraphicsEngine/src/Application.cpp 	modified:   GraphicsEngine/src/Renderer.cpp 	modified:   GraphicsEngine/src/Renderer.h 	modified:   GraphicsEngine/src/VertexArray.cpp 	modified:   GraphicsEngine/src/VertexArray.h

--- GraphicsEngine/src/Application.cpp
+++ GraphicsEngine/src/Application.cpp
@@ -10,6 +10,7 @@
 #include "IndexBuffer.h"
 #include "VertexArray.h"
 #include "Shader.h"
+#include "VertexBufferLayout.h"
 
 int main(void)
 {
@@ -88,6 +89,8 @@ int main(void)
         vb.Unbind();
         ib.Unbind();
         shader.Unbind();
+
+        Renderer renderer;
         
         float r = 0.0f;
         float increment = 0.01f;
@@ -95,15 +98,12 @@ int main(void)
         while (!glfwWindowShouldClose(window))
         {
             /* Render here */
-            GLCall(glClear(GL_COLOR_BUFFER_BIT));
+            renderer.Clear();
 
             shader.Bind();
             shader.SetUnifrom4f("u_Color", r, 0.0f, 1.0f, 1.0f);
 
-            va.Bind();
-            ib.Bind();
-
-            GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
+            renderer.Draw(va, ib, shader);
 
             if (r > 1.0f) {
                 increment = -0.01f;
--- GraphicsEngine/src/Renderer.h
+++ GraphicsEngine/src/Renderer.h
@@ -2,6 +2,9 @@
 
 #include<GL/glew.h>
 
+#include"VertexArray.h"
+#include"IndexBuffer.h"
+#include"Shader.h"
 
 #define ASSERT(x) if(!(x)) __debugbreak();
 #define GLCall(x) GLClearError();\
@@ -11,3 +14,9 @@
 void GLClearError();
 
 bool GLCheckError(const char* function, const char* file, int line);
+
+class Renderer {
+public:
+    void Clear() const;
+    void Draw(const VertexArray& va, const IndexBuffer& ib, const Shader& shader) const;
+};
